10650feeefd049cff17b74da33d2461c0c40491d
iamsuraj-01 <ssemwal181096@gmail.com>
2024-08-29 17:45:59 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,137 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate The wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+    
+    // Dropdown on mouse hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+    
+    $(window).on("load resize", function() {
+        if (this.matchMedia("(min-width: 992px)").matches) {
+            $dropdown.hover(
+            function() {
+                const $this = $(this);
+                $this.addClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "true");
+                $this.find($dropdownMenu).addClass(showClass);
+            },
+            function() {
+                const $this = $(this);
+                $this.removeClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "false");
+                $this.find($dropdownMenu).removeClass(showClass);
+            }
+            );
+        } else {
+            $dropdown.off("mouseenter mouseleave");
+        }
+    });
+
+
+    // Facts Counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+    
+    // Whatsapp Button
+    document.addEventListener("DOMContentLoaded", function() {
+        var whatsappLink = document.getElementById("whatsapp-link");
+        var phoneNumber = "916395525749"; // Your WhatsApp number
+        
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            // Mobile devices
+            whatsappLink.href = "https://wa.me/" + phoneNumber;
+        } else {
+            // Desktop devices
+            whatsappLink.href = "https://web.whatsapp.com/send?phone=" + phoneNumber;
+        }
+    });
+
+
+    // Testimonial Slider
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: true,
+        loop: true,
+        center: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+    // Vendor Slider
+    $('.vendor-carousel').owlCarousel({
+        loop: true,
+        margin: 45,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+            0:{
+                items:2
+            },
+            576:{
+                items:4
+            },
+            768:{
+                items:6
+            },
+            992:{
+                items:8
+            }
+        }
+    });
+
+    // Gallery Slider
+    $('#autoWidth').lightSlider({
+        autoWidth: true,
+        loop: true,
+        auto: true,             // Enable auto play
+        pause: 2000,            // Time in milliseconds between slides
+        speed: 700,             // Speed of the slide transition
+        onSliderLoad: function(){
+            $('#autoWidth').removeClass('cs-hidden');
+        }
+    });
+    
+})(jQuery);
+
